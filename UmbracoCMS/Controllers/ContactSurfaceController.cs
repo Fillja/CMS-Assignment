b756ff0552a07741bed3bdd8a09d6d4f22cf0fae
Robin Gombkoto <robin.gombkoto@gmail.com>
2024-09-17 16:12:45 +0200

    Forms completed and validated.

---
+++ UmbracoCMS/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Profiling.Internal;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+using UmbracoCMS.Models;
+
+namespace UmbracoCMS.Controllers;
+
+public class ContactSurfaceController : SurfaceController
+{
+    public ContactSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+    {
+    }
+
+    public IActionResult HandleSubmit(ContactFormModel form, bool isMessageRequired, bool isPhoneRequired)
+    {
+        if (!ModelState.IsValid || isMessageRequired && form.Message == null || isPhoneRequired && form.Phone == null) 
+        {
+            ViewData["name"] = form.Name;
+            ViewData["email"] = form.Email;
+            ViewData["phone"] = form.Phone;
+            ViewData["message"] = form.Message;
+
+            ViewData["error_name"] = string.IsNullOrEmpty(form.Name);
+            ViewData["error_email"] = string.IsNullOrEmpty(form.Email);
+            ViewData["error_phone"] = isPhoneRequired && string.IsNullOrEmpty(form.Phone);
+            ViewData["error_message"] = isMessageRequired && string.IsNullOrEmpty(form.Message);
+
+
+            return CurrentUmbracoPage();
+        }
+        TempData["success"] = "Form submitted successfully!";
+        return RedirectToCurrentUmbracoPage();
+    }
+}
---
+++ UmbracoCMS/Models/ContactFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UmbracoCMS.Models;
+
+public class ContactFormModel
+{
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string? Phone { get; set; }
+    public string? Message { get; set; }
+}
